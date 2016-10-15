3ed17b53a0113c19729318666983d925f2d06981
djsreeraj <sreeraj011m@gmail.com>
2016-10-15 20:38:42 +0530

    added icon,title,subtitle to toolbar

--- app/src/main/java/com/example/android/toolbardemo/MainActivity.java
+++ app/src/main/java/com/example/android/toolbardemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.toolbardemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(my_toolbar);
+        getSupportActionBar().setTitle(R.string.my_toolbar_title);
+        getSupportActionBar().setSubtitle(R.string.my_toolbar_subtitle);
+        getSupportActionBar().setIcon(R.drawable.ic_toolbar);
+
     }
 }

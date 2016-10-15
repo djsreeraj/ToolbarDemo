ef9db6b606a1db2827bf73efd4779fd417346691
djsreeraj <sreeraj011m@gmail.com>
2016-10-15 21:14:57 +0530

    added menu with options to toolbar

--- app/src/main/java/com/example/android/toolbardemo/MainActivity.java
+++ app/src/main/java/com/example/android/toolbardemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android.toolbardemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,29 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setSubtitle(R.string.my_toolbar_subtitle);
         getSupportActionBar().setIcon(R.drawable.ic_toolbar);
 
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.menu1:
+                Toast.makeText(MainActivity.this, "Edit Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu2:
+                Toast.makeText(MainActivity.this, "Go back", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
